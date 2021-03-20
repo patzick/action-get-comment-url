ab4749b1843685a2687eb0a361c6fe22d4919237
patzick <13100280+patzick@users.noreply.github.com>
2021-03-20 13:55:34 +0100

    chore: test two patterns

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { getInput } from "@actions/core";
 
 export const qwe: boolean = true;
 
+/**
+ * Example comment: 
+ * 
+ * 💙 shopware-pwa-canary successfully deployed at https://6bc7127fd9bef0a0b08bc14ea427fe5ddd986fde.shopware-pwa-canary.preview.storefrontcloud.io
+ */
+
 async function run() {
   const patterns = getInput("patterns")
 
